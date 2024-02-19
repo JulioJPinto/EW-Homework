b49b347d09ae80a236bf41255694d8ac041462ba
Júlio Pinto <pinto.julioj03@gmail.com>
2024-02-19 15:38:39 +0000

    stuff

--- TPC1/genpages.py
+++ TPC1/genpages.py
@@ -9,31 +9,28 @@ DATASET = "public/texto"
 def gen_html_pages(root, id):
     file = f"pages/{id}.html"
 
-    if not os.path.exists(file):
-        shutil.copyfile("ex-details.html", file)
+    shutil.copyfile("ex-details.html", file)
 
     nome = root.find('.//nome').text
 
     corpo = root.find('.//corpo')
 
-    figures = corpo.findall('.//figuras')
+    figures = root.findall('./figura')
     figures_div = gen_images(figures)
+    print(figures_div)
 
     houses_list = corpo.findall('.//lista-casas')
     houses_div = "\n"
     for lists  in houses_list:
         houses_div += gen_houses(lists) + "\n"
-        
-        
 
+    print(houses_div)
+        
     with open(file, 'r') as f:
         html_content = f.read()
 
-    print(html_content)
     modified_content = html_content.replace("{NAME}", nome)
-    modified_content.replace("{HOMES}", houses_div)
-    modified_content.replace("{FIGURES}", figures_div)
-    print(modified_content)
+    modified_content = modified_content.replace("{HOMES}", houses_div)
 
     with open(file, 'w') as f:
         f.write(modified_content)
@@ -41,15 +38,24 @@ def gen_html_pages(root, id):
     
 
 def gen_images(figures):
-    result = ""
+    result = "".replace("..", "../public")
     for f in figures:
+        img = f.find('./imagem')
+        caption = f.find('./legenda').text
         # Check if 'path' attribute exists
-        if 'path' in f.attrib:
-            img_src = f.attrib['path'].replace("..", "../public")
-            figure = f"""<div><img src="{img_src}" /></div>\n"""
+        if 'path' in img.attrib:
+            img_src = img.attrib.get('path', '')
+            img_src = img_src.replace("..", "../public")
+            figure = f"""<div>
+                            <img src="{img_src}" />
+                            <p>{caption}</p>
+                         </div>\n"""
             result += figure
-    return result
+        else:
+            print("Warning: Element has no 'path' attribute:", f.attrib)
 
+    print(result)
+    return result
 
 
 def gen_house(house):
@@ -64,16 +70,14 @@ def gen_house(house):
     desc = desc_element.text if desc_element is not None else "Unknown"
 
     res =f"""
-            <div class="justify-items-center p-2 border-2 rounded-sm border-gray-100">
-                <p>Número: {num}</p>
-                <p>Enfiteuta: {enfiteuta}</p>
-                <p>Foro: {foro}</p>
-                <p>Descrição:</p>
+            <div class="justify-items-center p-2 flex-auto border-2 rounded-lg border-gray-500">
+                <p><b>Número</b>: {num}</p>
+                <p><b>Enfiteuta</b>: {enfiteuta}</p>
+                <p><b>Foro</b>: {foro}</p>
+                <p><b>Descrição</b>:</p>
                 <p>{desc}</p>
             </div>
         """
-    
-    print(res)
     return res
 
 def gen_houses(houses):
